02f006863271607f08a94e95f66e559788a40fad
Hossam Tabana <hossam.tabana@gmail.com>
2023-10-07 07:04:00 +0200

    flask

--- 01_Python/13_Flask/get_data.py
+++ 01_Python/13_Flask/get_data.py
@@ -1,26 +1,41 @@
 import requests
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 BASE_URL = "https://idatafast.net/apptest"
 LOGIN_URL = f"{BASE_URL}/login"
 DATA_URL = f"{BASE_URL}/get_website_data"
 
 def authenticate_with_google():
-    # Note: This requires manual intervention unless automated using Selenium or another tool
-    browser = webdriver.Safari()
+    # Initialize the Selenium WebDriver
+    browser = webdriver.Safari() # Change this to your preferred driver
     browser.get(LOGIN_URL)
     
-    # You will need to add steps here to automate the login process using Selenium.
-    # This can be tricky since Google might detect and block automated login attempts.
+    # NOTE: Add code here to automate Google login
+    # This is a placeholder. Actual elements and flow can vary!
+    email_elem = browser.find_element(By.ID, 'identifierId')
+    email_elem.send_keys('hossam.tabana@gmail.com')
+    email_elem.send_keys(Keys.RETURN)
+    time.sleep(2)
+    
+    password_elem = browser.find_element(By.NAME, 'password')
+    password_elem.send_keys('your-password')
+    password_elem.send_keys(Keys.RETURN)
+    
+    time.sleep(5)  # Give some time for login to complete and to manually complete any CAPTCHAs or 2FA
     
     # After login is complete and you're redirected, you should have a session with the access token.
     cookies = browser.get_cookies()
-    browser.close()
+    browser.quit()
     return {cookie['name']: cookie['value'] for cookie in cookies}
 
 def get_data(cookies):
     response = requests.get(DATA_URL, cookies=cookies)
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Text: {response.text}")
     if response.status_code == 200:
         return pd.DataFrame(response.json())
     else:
@@ -28,6 +43,10 @@ def get_data(cookies):
 
 if __name__ == "__main__":
     cookies = authenticate_with_google()
-    data = get_data(cookies)
-    print(data)
-
+    print(f"Cookies: {cookies}")
+    
+    if cookies:
+        data = get_data(cookies)
+        print(data)
+    else:
+        print("Failed to authenticate.")
